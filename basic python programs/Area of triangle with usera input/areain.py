85d3c994ee6f3c30406f3fe3195eef70e4025a61
Kartik <Kartiksedha56@gmail.com>
2022-03-31 12:07:43 +0530

    Pyrhon Basic

---
+++ basic python programs/Area of triangle with usera input/areain.py
@@ -0,0 +1,18 @@
+# Program to find area of triangle with user input
+
+# To find area of triangle first we need to find semi-perimeter of triangle
+
+import cmath 
+a = float(input("Enter the value of first side "))
+b = float(input("Enter the value of second side "))
+c = float(input("Enter the value of third side "))
+# Here float is used because you can input both int value and float Value
+
+s = (a+b+c)/2  # Formula for finding semi-perimeter
+print('Semi-primeter of triangle is',s)
+
+
+# To find area of triangle we ha to find sq. root of -(s*(s-a)*(s-b)*(s-c))
+
+area = cmath.sqrt(s*(s-a)*(s-b)*(s-c))
+print("Area of triangle is " ,area)
